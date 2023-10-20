7a1e7d902f5f7b6af50001cf3e395726dd3b4aec
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-10-20 06:15:12 +0100

    feat(tls): add enable/disable tls test

--- tests/dragonfly/tls_conf_test.py
+++ tests/dragonfly/tls_conf_test.py
@@ -52,3 +52,51 @@ async def test_client_tls_cert(df_factory, with_tls_server_args):
     key_args.pop("tls")
     with df_factory.create(tls_replication=None, **key_args):
         pass
+
+
+async def test_config_enable_tls(
+    df_factory, with_ca_tls_server_args, with_tls_client_args, gen_ca_cert
+):
+    with df_factory.create() as server:
+        async with server.client() as client:
+            await client.ping()
+
+            # Note the order here matters as flags are applied in order.
+            await client.config_set(
+                "tls_key_file",
+                with_ca_tls_server_args["tls_key_file"],
+                "tls_cert_file",
+                with_ca_tls_server_args["tls_cert_file"],
+                "tls_ca_cert_file",
+                with_ca_tls_server_args["tls_ca_cert_file"],
+                "tls",
+                "true",
+            )
+
+            # The existing client should still be connected.
+            await client.ping()
+
+        # Connecting without TLS should fail.
+        with pytest.raises(redis.exceptions.ConnectionError):
+            async with server.client() as client_unauth:
+                await client_unauth.ping()
+
+        # Connecting with TLS should succeed.
+        async with server.client(
+            **with_tls_client_args, ssl_ca_certs=gen_ca_cert["ca_cert"]
+        ) as client_tls:
+            await client_tls.ping()
+
+
+async def test_config_disable_tls(
+    df_factory, with_ca_tls_server_args, with_tls_client_args, gen_ca_cert
+):
+    with df_factory.create(**with_ca_tls_server_args) as server:
+        async with server.client(
+            **with_tls_client_args, ssl_ca_certs=gen_ca_cert["ca_cert"]
+        ) as client_tls:
+            await client_tls.config_set("tls", "false")
+
+        # Connecting without TLS should succeed.
+        async with server.client() as client_unauth:
+            await client_unauth.ping()
